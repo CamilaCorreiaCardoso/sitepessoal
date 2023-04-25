238422d19f287f4879df1943d4c891463d0b084e
CamilaCorreiaCardoso <camilacorreiacardoso@hotmail.com>
2023-04-25 20:18:58 -0300

    aprendendo javascript

---
+++ JavaScript/learning.js
@@ -0,0 +1,40 @@
+// Tipos de dados
+//Strings (" algum texto" , 'algum texto')
+//Boolean ( true or false)
+//Number ( Int or float)
+//Object ( {} )
+//fuction
+
+//Strings
+a = "Seja bem vindo! "
+a = ' Seja bem vindo!'
+a = `Seja bem vindo!`
+
+HelloStr = "Hello"
+WorldStr = "World"
+HelloWorldStr = HelloStr + " " + WorldStr
+console.log(HelloWorldStr)
+
+//funções
+
+function soma(x, y) {
+    resultado = x + y
+    console.log(resultado)
+}
+
+function multiplicar(x, y) {
+    resultado = x * y
+    console.log(resultado)
+}
+
+function tresnumber(x, y, z) {
+    resultado = x + y + z
+    console.log(resultado)
+}
+
+function quadranumber(x, y, w, z) {
+    resultado = x + y + w + z
+
+    return resultado
+
+}
